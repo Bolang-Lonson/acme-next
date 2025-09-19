245d6e6fabd53fbcdb841dc1024ae01104821df3
Bolang-Lonson <bbeinyoh12@gmail.com>
2025-09-19 14:35:03 +0100

    project successfully initiated

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import '@/app/ui/global.css';
+
+export default function RootLayout({children,}: {children: React.ReactNode;}) {
+  return (
+    <html lang="en" data-google-analytics-opt-out="">
+      <body>{children}</body>
+    </html>
+  );
+}
